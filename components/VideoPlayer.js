6454cf1c3919bd25a9dcc46ce4c937472872a74a
christocarr <christocarr@outlook.com>
2021-01-19 15:28:41 +0000

    Fix: video url and iframe

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -1,16 +1,23 @@
-import ReactPlayer from 'react-player'
+import ReactPlayer from 'react-player/lazy'
 import styles from '../styles/VideoPlayer.module.css'
 
 function VideoPlayer({url}) {
   return (
     <div className={styles.player__wrapper}>
     
-      <ReactPlayer 
+      {/* <ReactPlayer 
         className={styles.player} 
         url={url}
         width="100%"
         height="100%"
-      />
+      /> */}
+      <iframe src={url}
+        frameborder='0'
+        allow='autoplay; encrypted-media'
+        allowfullscreen
+        title='video'
+/>
+    
     </div>
   )
 }
