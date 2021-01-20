9584c744cf0a6ed5143562d7c930717dbf6407af
christocarr <christocarr@outlook.com>
2021-01-20 12:43:31 +0000

    Chore: remove unused package

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -1,16 +1,8 @@
-import ReactPlayer from 'react-player/lazy'
 import styles from '../styles/VideoPlayer.module.css'
 
 function VideoPlayer({url}) {
   return (
     <div className={styles.player__wrapper}>
-    
-      {/* <ReactPlayer 
-        className={styles.player} 
-        url={url}
-        width="100%"
-        height="100%"
-      /> */}
       <iframe src={url}
         frameBorder='0'
         allow='autoplay; encrypted-media'
